3efad4a51b48cbb8412a12f2f8f18be2bd78cf11
21subham <subhamrajbhandari@gmail.com>
2024-11-30 17:53:47 +0545

    footer

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Header from "./header/Header";
-import Footer from "./footer/footer";
+import Footer from "./footer/Footer.jsx";
 import Container from "./container/Container";
 import LogoutBtn from "./header/LogoutBtn";
 import Button from "./Button";
