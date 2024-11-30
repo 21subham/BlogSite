ca57f5a6218952558cf03b47cadecedfa7775fa4
21subham <subhamrajbhandari@gmail.com>
2024-11-30 21:20:59 +0545

    postcard

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import LogoutBtn from "./header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
 import Login from "./Login";
-import PostCard from "./PostCard.jsx";
+import PostCard from "../components/PostCard";
 import AuthLayout from "./AuthLayout";
 import Select from "./Select";
 import RTE from "./RTE";
