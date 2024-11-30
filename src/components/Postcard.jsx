66a3ef7ed8f2e8e4033b9fc935c7fca291750ebc
21subham <subhamrajbhandari@gmail.com>
2024-11-30 18:58:24 +0545

    monor changes

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -2,7 +2,7 @@ import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-function Postcard({ $id, title, featuredImage }) {
+function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4  w-800">
@@ -19,10 +19,10 @@ function Postcard({ $id, title, featuredImage }) {
   );
 }
 
-Postcard.propTypes = {
+PostCard.propTypes = {
   $id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   featuredImage: PropTypes.string.isRequired,
 };
 
-export default Postcard;
+export default PostCard;
